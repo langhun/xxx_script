7e0e9e5c887e70d165901a248328a9bae2fd64ad
langhun <langhun@gmail.com>
2022-05-30 16:46:28 +0800

    Update jd_gua_JDnhjRed.js

--- jd_gua_JDnhjRed.js
+++ jd_gua_JDnhjRed.js
@@ -3,12 +3,12 @@
 cron 0 0,10,20,22 * * *  https://raw.githubusercontent.com/smiek2121/scripts/master/gua_nhjRed.js
 整点跑 红包几率大点
 
-https://u.jd.com/JtxypHO
+https://u.jd.com/JI9TBFd
 跳转到app 可查看助力情况
 
 返利变量：gua_nhjRed_rebateCode，若需要返利给自己，请自己修改环境变量[gua_nhjRed_rebateCode]
 Stu3HYc换成自己的返利
-export gua_nhjRed_rebateCode="JtxypHO"
+export gua_nhjRed_rebateCode="JI9TBFd"
 
 需要助力[火力值]的账号pin值
 如：【京东账号2】pin
@@ -19,7 +19,7 @@ export gua_nhjRed_rebatePin="pin1,pin2"
 
 */
 
-let rebateCodes = 'JtxypHO'
+let rebateCodes = 'JI9TBFd'
 let rebatePin = ''
 
 const $ = new Env('年货节红包');
